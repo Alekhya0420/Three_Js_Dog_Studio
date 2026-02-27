347d2adac8065eb9feebb51b40dbe421ee6d2a87
Alekhya Roy <alekhya.roy@webskitters.com>
2026-02-27 19:24:23 +0530

    Three js dog studio setup

---
+++ src/components/Dog.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react-hooks/immutability */
+import React, { useEffect } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useGLTF,
+  useTexture,
+  useAnimations,
+} from "@react-three/drei";
+import * as THREE from "three";
+
+const Dog = () => {
+  const model = useGLTF("/models/dog.drc.glb");
+  useThree(({ camera, gl }) => {
+    camera.position.z = 0.66;
+    gl.toneMapping = THREE.ReinhardToneMapping;
+    gl.outputColorSpace = THREE.SRGBColorSpace;
+  });
+
+  const { actions } = useAnimations(model.animations, model.scene);
+
+  useEffect(()=>{
+    actions["Take 001"].play()
+  },[actions])
+
+  const [normalMap, sampleMatcap,branchMap,branchNormalMap] = useTexture([
+    "/dog_normals.jpg",
+    "/matcap/mat-2.png",
+    "/branches_diffuse.jpeg",
+    "/branches_normals.jpeg"
+  ]).map((texture) => {
+    texture.flipY = false;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  const dogMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    matcap: sampleMatcap,
+  });
+
+  const branchMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    map: branchMap,
+  });
+
+  model.scene.traverse((child) => {
+    if (child.name.includes("DOG")) {
+      child.material = dogMaterial;
+    }
+    else
+    {
+      child.material =branchMaterial
+    }
+  });
+  return (
+    <>
+      <primitive
+        object={model.scene}
+        position={[0.25, -0.55, 0]}
+        rotation={[0, Math.PI / 3.9, 0]}
+      />
+      <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
+      <OrbitControls />
+    </>
+  );
+};
+
+export default Dog;
